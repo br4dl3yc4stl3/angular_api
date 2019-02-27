edbfd759ad3248de36a383b615845d0fff005672
br4dl3yc4stl3 <47078776+br4dl3yc4stl3@users.noreply.github.com>
2019-02-27 20:16:40 +0900

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('listApp', [])
     .controller('ListController', ['$http', function($http) {
-        $http.get("https://www.eventbriteapi.com/v3/events/search/?token=53IOCM3XYDRTUADO5LY&location.address=london+uk")
+        $http.get("https://www.eventbriteapi.com/v3/events/search/?token=53IOCM3XYDRTUADO5LYE&location.address=london+uk")
         .then(function(data) {
             console.log(data);
         });
